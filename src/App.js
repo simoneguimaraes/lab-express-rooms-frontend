bf28237e68ce2764388f5680589561a6fb3d00bc
Simone Guimaraes <simoneguimaraes11@gmail.com>
2021-12-07 08:50:25 -0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Home from "./components/pages/Home";
+import axios from "axios";
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
